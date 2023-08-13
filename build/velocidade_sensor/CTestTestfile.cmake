27b085a09699a326fec39a66e0b10cd421eb913d
Bruna Schreiber <140959409+schreiber-bruna@users.noreply.github.com>
2023-08-13 00:34:06 -0300

    Add files via upload

---
+++ build/velocidade_sensor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bruna/velocidades_ws/src/velocidade_sensor
+# Build directory: /home/bruna/velocidades_ws/build/velocidade_sensor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
