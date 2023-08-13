a17375e0a079a9d17a4c2e8fa8f4b0bdd60e5438
Bruna Schreiber <140959409+schreiber-bruna@users.noreply.github.com>
2023-08-13 01:19:13 -0300

    sistema_solar.py

--- entregável ros avançado/final_ws/src/sistema_solar/scripts/sistema_solar.py
+++ entregável ros avançado/final_ws/src/sistema_solar/scripts/sistema_solar.py
@@ -4,38 +4,38 @@ import rospy
 from geometry_msgs.msg import TransformStamped
 import math
 
-def planeta_transformacao(angle, radius): 
+def planeta_transformacao(angulo, radianos): 
     transform = TransformStamped()
     transform.header.stamp = rospy.Time.now()
-    transform.header.frame_id = "estrela"
-    transform.child_frame_id = "planeta"
-    transform.transform.translation.x = radius * math.cos(angle)
-    transform.transform.translation.y = radius * math.sin(angle)
+    transform.header.frame_id = "estrela" #coordenada de referencia
+    transform.child_frame_id = "planeta" #coordenada q está sendo transformado em relaçao ao de referencia
+    transform.transform.translation.x = radianos * math.cos(angulo) #translação ao longo do eixo x
+    transform.transform.translation.y = radianos * math.sin(angulo) #translação ao longo do eixo y
     transform.transform.rotation.w = 1.0
-    return transform
+    return transform 
 
-def satelite_transformacao(angle, radius): 
+def satelite_transformacao(angulo, radianos): 
     transform = TransformStamped()
     transform.header.stamp = rospy.Time.now()
-    transform.header.frame_id = "estrela"
-    transform.child_frame_id = "satelite"
-    transform.transform.translation.x = radius * math.cos(angle)
-    transform.transform.translation.y = radius * math.sin(angle)
+    transform.header.frame_id = "estrela" #coordenada de referencia
+    transform.child_frame_id = "satelite" #coordenada q está sendo transformada em relacao ao de referencia
+    transform.transform.translation.x = radianos * math.cos(angulo) #translação ao longo do eixo x
+    transform.transform.translation.y = radianos * math.sin(angulo) #translação ao longo do eixo y
     transform.transform.rotation.w = 1.0
     return transform
 
 if __name__ == '__main__':
     rospy.init_node('sistema_solar')
-    broadcaster = rospy.Publisher('sistema_solar_transform', TransformStamped, queue_size=10)
+    broadcaster = rospy.Publisher('sistema_solar_transformacao', TransformStamped, queue_size=10) #cria um publisher para emviar mensagens fo tipo TRansformStamped para o sistema solar
     rate = rospy.Rate(10)  # 10 Hz
 
     planeta_radianos = rospy.get_param("~planeta_radianos")
     satelite_radianos = rospy.get_param("~satelite_radianos")
 
     while not rospy.is_shutdown():
-        time = rospy.Time.now()
+        time = rospy.Time.now() #calcular o angulo q usará na transf
 
-        planeta_angulo = time.to_sec() * math.pi / 5.0
+        planeta_angulo = time.to_sec() * math.pi / 5.0 #calcula o angulo no tempo atual para q o angulo varie com o tempo
         planeta_transformacao = planeta_transformacao(planeta_angulo, planeta_radianos)
         broadcaster.publish(planeta_transformacao)
 
