8cf6c5859803d3acb668850fb33f1d8bc1f645d5
Bruna Schreiber <140959409+schreiber-bruna@users.noreply.github.com>
2023-08-13 00:36:58 -0300

    Add files via upload

---
+++ ros basico/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/bruna/velocidades_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/bruna/velocidade_ws/devel;/home/bruna/final_ws/devel;/home/bruna/projeto_ws/devel'.split(';') if '/home/bruna/velocidade_ws/devel;/home/bruna/final_ws/devel;/home/bruna/projeto_ws/devel' != '' else []
