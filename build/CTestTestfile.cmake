6d1034b96a2e97cbc8f3dbf7d24e6655ce9b5fd0
Bruna Schreiber <140959409+schreiber-bruna@users.noreply.github.com>
2023-08-13 00:35:06 -0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bruna/velocidades_ws/src
-# Build directory: /home/bruna/velocidades_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("velocidade_sensor")
--- build/velocidade_sensor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bruna/velocidades_ws/src/velocidade_sensor
-# Build directory: /home/bruna/velocidades_ws/build/velocidade_sensor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
